ccf56d45b854490e298e5d15ef5a66354658c5ba
?pit_pranks <123305625+devpit@users.noreply.github.com>
2024-11-03 19:21:56 -0300

    Update pitscrapy.py
    hello

--- pitscrapy.py
+++ pitscrapy.py
@@ -4,17 +4,44 @@ import argparse
 import requests
 from bs4 import BeautifulSoup
 from colorama import init, Fore
+from urllib.parse import urlparse
+from requests.exceptions import Timeout, RequestException
+import logging
 
 init(autoreset=True)
 
-def get_soup(url):
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
+}
+
+def get_session():
+    session = requests.Session()
+    session.headers.update(headers) 
+    return session
+
+def validate_url(url):
+    parsed_url = urlparse(url)
+    return parsed_url.scheme in ['http', 'https'] and bool(parsed_url.netloc)
+
+def fetch_url(url, session, timeout):
     try:
-        response = requests.get(url)
+        response = session.get(url, timeout=timeout, allow_redirects=True)
         response.raise_for_status()
-        return BeautifulSoup(response.content, 'html.parser')
-    except requests.exceptions.RequestException as e:
-        print(Fore.RED + f"Request failed: {e}")
+        return response.content
+    except Timeout:
+        logging.error(Fore.RED + f"Request timed out for URL: {url}.")
         return None
+    except RequestException as e:
+        logging.error(Fore.RED + f"Request failed for URL: {url} - {e}.")
+        return None
+
+def get_soup(url, session, timeout):
+    content = fetch_url(url, session, timeout)
+    if content:
+        return BeautifulSoup(content, 'html.parser')
+    return None
 
 def extract_links(soup):
     return [link.get('href') for link in soup.find_all('a', href=True)] if soup else []
@@ -26,186 +53,107 @@ def extract_videos(soup):
     return [video.get('src') for video in soup.find_all('video', src=True)] if soup else []
 
 def extract_scripts(soup):
-    if soup:
-        scripts_with_src = [script.get('src') for script in soup.find_all('script', src=True)]
-        scripts_without_src = [script.string.strip() for script in soup.find_all('script') if script.string and not script.get('src')]
-        return scripts_with_src, scripts_without_src
-    return [], []
+    scripts_with_src = [script.get('src') for script in soup.find_all('script', src=True)]
+    scripts_without_src = [script.string.strip() for script in soup.find_all('script') if script.string and not script.get('src')]
+    return scripts_with_src, scripts_without_src
 
-def save_to_file(data, file_path):
-    with open(file_path, 'w', encoding='utf-8') as file:
-        for item in data:
-            file.write("%s\n" % item)
+def save_data_to_file(data, file_path):
+    try:
+        with open(file_path, 'w', encoding='utf-8') as file:
+            for item in data:
+                file.write(f"{item}\n")
+        logging.info(Fore.GREEN + f"Data saved to {file_path}.")
+    except IOError as e:
+        logging.error(Fore.RED + f"Failed to save to {file_path}: {e}")
 
 def create_directory(directory):
-    if not os.path.exists(directory):
-        os.makedirs(directory)
-
-def extract_source_code(url, output_directory=None, save_to_file=False):
     try:
-        response = requests.get(url)
-        response.raise_for_status()
-        content = response.text
-        if save_to_file:
+        os.makedirs(directory, exist_ok=True)
+        logging.info(Fore.GREEN + f"Directory created: {directory}.")
+    except OSError as e:
+        logging.error(Fore.RED + f"Failed to create directory {directory}: {e}")
+
+def extract_source_code(url, session, output_directory=None, save_to_file=False, timeout=10):
+    try:
+        content = fetch_url(url, session, timeout)
+        if save_to_file and content:
             create_directory(output_directory)
             file_path = os.path.join(output_directory, "source_code.html")
-            with open(file_path, "w", encoding="utf-8") as file:
-                file.write(content)
-            print(Fore.GREEN + f"Source code saved in '{file_path}'")
-        else:
-            print(Fore.GREEN + content)
-    except requests.exceptions.RequestException as e:
-        print(Fore.RED + f"Request failed: {e}")
+            save_data_to_file([content.decode('utf-8')], file_path)
+            logging.info(Fore.GREEN + f"Source code saved in '{file_path}'")
+        elif content:
+            logging.info(Fore.RED + f"Source code extracted for {url}.")
+            print(content.decode('utf-8'))
+    except Exception as e:
+        logging.error(Fore.RED + f"Failed to extract source code: {e}")
 
-def extract_all(url):
-    soup = get_soup(url)
+def extract_all(url, session, timeout):
+    soup = get_soup(url, session, timeout)
     if not soup:
         return
 
+    scripts_with_src, scripts_without_src = extract_scripts(soup)
     data = {
         "links": extract_links(soup),
         "images": extract_images(soup),
         "videos": extract_videos(soup),
-        "scripts_with_src": extract_scripts(soup)[0],
-        "scripts_without_src": extract_scripts(soup)[1],
+        "scripts_with_src": scripts_with_src,
+        "scripts_without_src": scripts_without_src,
     }
     
-    output_directory = "data_found"
+    output_directory = "pits_found"
     create_directory(output_directory)
     for key, value in data.items():
         if value:
             dir_path = os.path.join(output_directory, key)
             create_directory(dir_path)
             file_path = os.path.join(dir_path, f"{key}_found.txt" if key != "scripts_without_src" else f"{key}_found.js")
-            save_to_file(value, file_path)
-            print(Fore.GREEN + f"{key.capitalize()} saved in: {file_path}")
+            save_data_to_file(value, file_path)
         else:
-            print(Fore.RED + f"No {key} found.")
-    extract_source_code(url, output_directory, save_to_file=True)
-
-def display_help():
-    help_text = """  
-    Pit Scrapy | Massive Data Colect - Usage:
-    - Enter the target URL when prompted.
-    - Choose what you want to extract:
-        -l : Extract links
-        -i : Extract images
-        -v : Extract videos
-        -sc : Extract source code and display it
-        -scs : Extract source code and save to file
-        -sws : Extract scripts with src attribute
-        -swss : Extract scripts without src attribute
-        -all : Extract everything at once
-        
-    Example:
-    pitscrapy.py -u http://example.com -l -i -v
-
-    Credits:
-    Developed by the DevPit
-    https://github.com/devpit/pitscrapy
-    """
-    print(help_text)
+            logging.warning(Fore.RED + f"No {key} found.")
+    
+    extract_source_code(url, session, output_directory, save_to_file=True, timeout=timeout)
 
 def display_ascii_art():
     ascii_art = """
-    #####    ######   ######    ####     ####    #####      ##     #####    ##  ##
-    ##  ##     ##       ##     ##       ##  ##   ##  ##    ####    ##  ##   ##  ##
-    ##  ##     ##       ##      ####    ##       ##  ##   ##  ##   ##  ##    ####
-    #####      ##       ##         ##   ##       #####    ##  ##   #####      ##
-    ##         ##       ##         ##   ##  ##   ## ##    ######   ##         ##
-    ##       ######     ##      ####     ####    ##  ##   ##  ##   ##         ##                          
+    ██████  ██ ████████ ███████  ██████ ██████   █████  ██████  ██    ██ 
+    ██   ██ ██    ██    ██      ██      ██   ██ ██   ██ ██   ██  ██  ██  
+    ██████  ██    ██    ███████ ██      ██████  ███████ ██████    ████   
+    ██      ██    ██         ██ ██      ██   ██ ██   ██ ██         ██    
+    ██      ██    ██    ███████  ██████ ██   ██ ██   ██ ██         ██      
+    Developed by Pit
+    https://github.com/devpit/pitscrapy                                                                         
     """
     print(Fore.GREEN + ascii_art)
 
 def main():
-    parser = argparse.ArgumentParser(description="Pit Scrapy - Massive Data Colect", add_help=False)
-    parser.add_argument('-u', '--url', required=False, help='Target URL')
-    parser.add_argument('-l', '--links', action='store_true', help='Extract links')
-    parser.add_argument('-i', '--images', action='store_true', help='Extract images')
-    parser.add_argument('-v', '--videos', action='store_true', help='Extract videos')
-    parser.add_argument('-sc', '--source-code', action='store_true', help='Extract and display source code')
-    parser.add_argument('-scs', '--source-code-save', action='store_true', help='Extract and save source code to file')
-    parser.add_argument('-sws', '--scripts-with-src', action='store_true', help='Extract scripts with src attribute')
-    parser.add_argument('-swss', '--scripts-without-src', action='store_true', help='Extract scripts without src attribute')
-    parser.add_argument('-all', '--all', action='store_true', help='Extract everything at once')
-    parser.add_argument('-h', '--help', action='store_true', help='Show this help message and exit')
+    display_ascii_art()
+    session = get_session()
+
+    parser = argparse.ArgumentParser(description="A web scraper to extract links, images, videos, scripts, and source code from a webpage.")
+    parser.add_argument('-u', '--url', type=str, required=True, help='URL of the webpage to scrape.')
 
     args = parser.parse_args()
-
-    if args.help:
-        display_ascii_art()
-        display_help()
-        sys.exit(0)
-
-    if not args.url:
-        print(Fore.RED + "No URL provided!")
-        display_ascii_art()
-        display_help()
-        sys.exit(1)
-
-    display_ascii_art()
-
-    if not (args.links or args.images or args.videos or args.source_code or args.source_code_save or args.scripts_with_src or args.scripts_without_src or args.all):
-        print(Fore.RED + "No extraction option was provided!")
-        display_help()
-        sys.exit(1)
-
     url = args.url
-    soup = get_soup(url)
 
-    if args.all:
-        extract_all(url)
+    if validate_url(url):
+        extract_all(url, session, timeout=10)
     else:
-        if args.links:
-            links = extract_links(soup)
-            if links:
-                create_directory("links")
-                save_to_file(links, "links/links_found.txt")
-                print(Fore.GREEN + "Links saved in 'links/links_found.txt'")
-            else:
-                print(Fore.RED + "No links found.")
-        
-        if args.images:
-            images = extract_images(soup)
-            if images:
-                create_directory("images")
-                save_to_file(images, "images/images_found.txt")
-                print(Fore.GREEN + "Images saved in 'images/images_found.txt'")
-            else:
-                print(Fore.RED + "No images found.")
-        
-        if args.videos:
-            videos = extract_videos(soup)
-            if videos:
-                create_directory("videos")
-                save_to_file(videos, "videos/videos_found.txt")
-                print(Fore.GREEN + "Videos saved in 'videos/videos_found.txt'")
-            else:
-                print(Fore.RED + "No videos found.")
-        
-        if args.source_code:
-            extract_source_code(url)
-        
-        if args.source_code_save:
-            extract_source_code(url, output_directory="source_code", save_to_file=True)
-        
-        if args.scripts_with_src or args.scripts_without_src:
-            scripts_with_src, scripts_without_src = extract_scripts(soup)
-            if args.scripts_with_src:
-                if scripts_with_src:
-                    create_directory("scripts")
-                    save_to_file(scripts_with_src, "scripts/scripts_with_src_found.txt")
-                    print(Fore.GREEN + "Scripts with src attribute saved in 'scripts/scripts_with_src_found.txt'")
-                else:
-                    print(Fore.RED + "No scripts with src attribute found.")
-            
-            if args.scripts_without_src:
-                if scripts_without_src:
-                    create_directory("scripts")
-                    save_to_file(scripts_without_src, "scripts/scripts_without_src_found.js")
-                    print(Fore.GREEN + "Scripts without src attribute saved in 'scripts/scripts_without_src_found.js'")
-                else:
-                    print(Fore.RED + "No scripts without src attribute found.")
+        logging.error(Fore.RED + "Invalid URL provided!")
 
-if __name__ == "__main__":
+    while True:
+        new_url = input(Fore.WHITE + "Do you want to check a new URL? (yes to continue, type 'exit' to quit): ").strip().lower()
+        if new_url == 'exit':
+            print(Fore.RED + "Exiting the program.")
+            break
+        elif new_url == 'yes':
+            url = input(Fore.WHITE + "Enter the new URL: ").strip()
+            if validate_url(url):
+                extract_all(url, session, timeout=10)
+            else:
+                logging.error(Fore.RED + "Invalid URL provided!")
+        else:
+            logging.error(Fore.RED + "Please type 'yes' to continue or 'exit' to quit.")
+
+if __name__ == '__main__':
     main()
